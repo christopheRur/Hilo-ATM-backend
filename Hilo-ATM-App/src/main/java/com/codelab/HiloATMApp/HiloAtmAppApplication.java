16fb3717ffbb7b5d236b61053b3ae0ebcd880e95
Rurangwa <chrisruu@yahoo.com>
2023-12-29 12:01:55 -0500

    initial commit

---
+++ Hilo-ATM-App/src/main/java/com/codelab/HiloATMApp/HiloAtmAppApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.HiloATMApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiloAtmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiloAtmAppApplication.class, args);
+	}
+
+}
---
+++ Hilo-ATM-App/src/test/java/com/codelab/HiloATMApp/HiloAtmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.HiloATMApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiloAtmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
